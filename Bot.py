3ddc30f0a2fe53c1ae3d125f7fdfc238cb94b55d
Lawrence <DenouementX@users.noreply.github.com>
2018-04-20 11:56:36 -0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -202,5 +202,4 @@ async def on_message(message):
         sqgraph(str1)
         await client.send_file(message.channel, 'stats.png')
 
-client.run("NDM1ODg0ODQ0NDM4OTEzMDI0.Dbf8bw.pW-ZzrgheLGCJjfTS0YmCBUIbzA")
 client.login(process.env.BOT_TOKEN);
